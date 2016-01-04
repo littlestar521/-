cebec9c59f2c06426a6db7642add141c403924a9
马娟娟 <2891529590@qq.com>
2016-01-04 11:23:23 +0800

    first

---
+++ 最美周末/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  最美周末
+//
+//  Created by scjy on 16/1/4.
+//  Copyright © 2016年 马娟娟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
